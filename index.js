9fc33f587e4a2873c62fc215d307de5ab71e14c9
park_so_young <soyoung931014@gmail.com>
2022-08-11 21:49:18 +0900

    배포

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const path = require("path");
 const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
